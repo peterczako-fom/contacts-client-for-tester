42466a241fb9194254eded6d81a84b047c3ac4b9
CzakoPeter <peter.czako@fututreofmedia.com>
2022-05-10 11:44:41 +0200

    add fields

--- src/app/models/contact-dto.ts
+++ src/app/models/contact-dto.ts
@@ -1,2 +1,11 @@
 export interface ContactDto {
+  id: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+  phoneNumber: string;
+  companyId: number;
+  comment: string;
+  createdDate: string;
+  lastModifiedDate: string;
 }
